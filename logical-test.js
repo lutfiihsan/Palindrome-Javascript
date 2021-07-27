cb0b575da78ddcb90848007395818a3de82f1c83
Lutfi Ihsan <lutficreativesys@gmail.com>
2021-07-27 15:48:14 +0700

    final result

--- logical-test.js
+++ logical-test.js
@@ -1,22 +1,21 @@
 function changetNumberToText(){
-    let data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
-    let result = [];
+    // let data = 50
+    let result = '';
+    let results = [];
 
-    for(let i = 0; i<data.length; i++){
-        if(data[i]%3 == 0){
-            data[i] = 'Frontend';
+    for(let i = 1; i<=50; i++){
+        if(i%15==0){
+            result='Frontend Backend'
+        }else if(i%3==0){
+            result='Frontend'
+        }else if(i%5==0){
+            result='Backend'
+        }else{
+            result = i
         }
-        if(data[i]%5 == 0){
-            data[i] = 'Backend';
-        }
-        if(data[i]%15 == 0){
-            data[i] == 'Frontend Backend';
-        }
-        console.log(data[i]);
-        // result = data[i];
-        result.push(data[i]);
+        results.push(result)
     }
-    console.log(result);
+    console.log(results)
 }
 
 console.log(changetNumberToText())
